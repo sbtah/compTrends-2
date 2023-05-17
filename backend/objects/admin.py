2e49626060f07ede7f334f72d87baba441e23994
Sbtah <shisastore@protonmail.com>
2023-05-17 22:57:53 +0200

    Added Task object to admin.

--- backend/objects/admin.py
+++ backend/objects/admin.py
@@ -1,19 +1,9 @@
 from django.contrib import admin
 from objects.models.stores import EcommerceStore, LocalStore
-from objects.models.tasks import (
-    CrawlLocalStores,
-    CrawlCategories,
-    CrawlProducts,
-    ScrapeProduct,
-    ScrapeLocalProductData
-)
+from objects.models.tasks import Task
 
 
 admin.site.register(EcommerceStore)
 admin.site.register(LocalStore)
 
-admin.site.register(CrawlLocalStores)
-admin.site.register(CrawlCategories)
-admin.site.register(CrawlProducts)
-admin.site.register(ScrapeProduct)
-admin.site.register(ScrapeLocalProductData)
+admin.site.register(Task)
