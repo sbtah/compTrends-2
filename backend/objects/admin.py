c417109082943bdf0c7c7aef3da0ce9f0e5c5a9b
Sbtah <shisastore@protonmail.com>
2023-05-17 21:26:02 +0200

    Registered new models.

--- backend/objects/admin.py
+++ backend/objects/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from objects.models.stores import EcommerceStore, LocalStore
-from objects.models.tasks import CrawlCategories, CrawlProducts, ScrapeProduct
+from objects.models.tasks import (
+    CrawlLocalStores,
+    CrawlCategories,
+    CrawlProducts,
+    ScrapeProduct,
+    ScrapeLocalProductData
+)
 
 
 admin.site.register(EcommerceStore)
 admin.site.register(LocalStore)
 
+admin.site.register(CrawlLocalStores)
 admin.site.register(CrawlCategories)
 admin.site.register(CrawlProducts)
 admin.site.register(ScrapeProduct)
+admin.site.register(ScrapeLocalProductData)
