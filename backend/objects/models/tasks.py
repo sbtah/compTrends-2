6d5e2816e84135cf1b9ef3c285ebea3380d72bdc
Sbtah <shisastore@protonmail.com>
2023-05-17 22:58:19 +0200

    Refactored Tasks to use only 1 model.

--- backend/objects/models/tasks.py
+++ backend/objects/models/tasks.py
@@ -11,112 +11,32 @@ class Task(models.Model):
         extended = 'FAILED'
         finished = 'FINISHED'
 
-    store = models.ForeignKey(EcommerceStore, on_delete=models.PROTECT)
-    url = models.CharField(max_length=255)
+    class Type(models.TextChoices):
+        local_stores_crawl = 'STORES-CRAWL'
+        categories_crawl = 'CATEGORIES-CRAWL'
+        products_crawl = 'PRODUCTS-CRAWL'
+        products_scrape = 'PRODUCT-SCRAPE'
+        product_local_data_scrape = 'PRODUCTLOCALDATA-SCRAPE'
+
+
+    store = models.ForeignKey(
+        EcommerceStore,
+        on_delete=models.PROTECT
+    )
+    url = models.URLField(max_length=255)
     status = models.CharField(
         max_length=10,
         choices=Status.choices,
         default=Status.created,
     )
+    type = models.CharField(
+        max_length=40,
+        choices=Type.choices,
+    )
+    category_level = models.IntegerField(blank=True, null=True)
     created = models.IntegerField()
     started_at = models.IntegerField(blank=True, null=True)
     finished_at = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        abstract = True
-
-
-class CrawlLocalStores(Task):
-    '''Class for CrawlLocalStores Task object.'''
-
-    class Type(models.TextChoices):
-        stores_crawl = 'STORES CRAWL'
-    type = models.CharField(
-        max_length=30,
-        choices=Type.choices,
-        default=Type.stores_crawl,
-    )
-
-    class Meta:
-        verbose_name_plural = 'CrawlLocalStores Tasks'
-
-    def __str__(self):
-        return f'{self.type}: {self.url}'
-
-
-class CrawlCategories(Task):
-    '''Class for CrawlCategories task object.'''
-
-    class Type(models.TextChoices):
-        categories_crawl = 'CATEGORIES CRAWL'
-
-    type = models.CharField(
-        max_length=30,
-        choices=Type.choices,
-        default=Type.categories_crawl,
-    )
-    category_level = models.IntegerField()
-
-    class Meta:
-        verbose_name_plural = 'CrawlCategories Tasks'
-
-    def __str__(self):
-        return f'{self.type}: {self.url}'
-
-
-class CrawlProducts(Task):
-    '''Class for CrawlProducts task object.'''
-
-    class Type(models.TextChoices):
-        products_crawl = 'PRODUCTS CRAWL'
-
-    type = models.CharField(
-        max_length=30,
-        choices=Type.choices,
-        default=Type.products_crawl,
-    )
-    category_level = models.IntegerField()
-
-    class Meta:
-        verbose_name_plural = 'CrawlProducts Tasks'
-
-    def __str__(self):
-        return f'{self.type}: {self.url}'
-
-
-class ScrapeProduct(Task):
-    '''Class for ScrapeProduct task objects.'''
-
-    class Type(models.TextChoices):
-        product_scrape = 'PRODUCT SCRAPE'
-
-    type = models.CharField(
-        max_length=30,
-        choices=Type.choices,
-        default=Type.product_scrape,
-    )
-
-    class Meta:
-        verbose_name_plural = 'ScrapeProducts Tasks'
-
-    def __str__(self):
-        return f'{self.type}: {self.url}'
-
-
-class ScrapeLocalProductData(Task):
-    '''Class for ScrapeLocalProductData task objects.'''
-
-    class Type(models.TextChoices):
-        product_local_scrape = 'PRODUCT LOCAL DATA SCRAPE'
-
-    type = models.CharField(
-        max_length=45,
-        choices=Type.choices,
-        default=Type.product_local_scrape,
-    )
-
-    class Meta:
-        verbose_name_plural = 'ScrapeLocalProductData Tasks'
 
     def __str__(self):
         return f'{self.type}: {self.url}'
