df353caf7e15f8cb6bc7c6c87f5d55314451ea37
Sbtah <shisastore@protonmail.com>
2023-05-17 21:57:12 +0200

    Added create_initial_discovery_tasks task.

--- backend/tasks/tasks.py
+++ backend/tasks/tasks.py
@@ -1,28 +1,24 @@
 import time
 
 from celery import shared_task
-from objects.models.stores import EccommerceStore
+from objects.models.stores import EcommerceStore
+from objects.models.tasks import CrawlCategories
 
 
+# TODO:
+# Delete aftert testing.
 @shared_task(name='initial_discovery_task')
 def initial_discovery():
     from scraper.stores.leroy_merlin.leroy_merlin import LeroyMerlinScraper
-    with LeroyMerlinScraper(requested_url="https://www.new.leroymerlin.pl/", store_url="https://www.leroymerlin.pl/") as scraper:
+    with LeroyMerlinScraper(
+            requested_url="https://www.new.leroymerlin.pl/",
+            store_url="https://www.leroymerlin.pl/"
+        ) as scraper:
         element = scraper.visit_page(url=scraper.requested_url)
         generator = scraper.find_categories_data(category_level=0, html_element=element)
         for cat in generator:
             print(cat)
         scraper.quit_and_clean()
-
-
-@shared_task(name='discovery_task')
-def discovery_task():
-    # discovery_group = group(
-    #     [initial_discovery() for _ in range(12)]
-    #     ).apply_async()
-    for _ in range(12):
-        initial_discovery.apply_async()
-    # return discovery_group
 
 
 @shared_task(
@@ -32,8 +28,15 @@ def discovery_task():
         name='create_initial_discovery_tasks',
     )
 def create_initial_discovery_tasks(self):
-    active_stores = EccommerceStore.objects.filter(is_active=True)
+    active_stores = EcommerceStore.objects.filter(is_active=True)
     for store in active_stores:
-        created = time.time()
-        task = TaskObject.objects.create(store=store, status='CREATED', type='CRAWL', created=created)
+        created = int(time.time())
+        task = CrawlCategories.objects.create(
+            store=store,
+            url=store.discovery_url,
+            status='CREATED',
+            type='CRAWL',
+            category_level=0,
+            created=created
+            )
     return task.id
